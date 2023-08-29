499b66d4ef3edea5c0154f45975531fa3094b738
duyhungg <dongocduyhung7@gmail.com>
2023-08-29 12:17:47 +0700

    [Backend]- create model

---
+++ tlcn-cnpm-be/Backend/config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectDatabase = () => {
+  mongoose
+    .connect(process.env.DB_LOCAL_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      //family: 4, // Use IPv4, skip trying IPv6
+    })
+    .then((con) => {
+      console.log(
+        `MongoDB Database connected with HOST: ${con.connection.host}`
+      );
+    });
+};
+
+module.exports = connectDatabase;
--- tlcn-cnpm-be/Backend/server.js
+++ tlcn-cnpm-be/Backend/server.js
@@ -1,9 +1,11 @@
 const app = require("./app");
-
+const connectDatabase = require("./config/database");
 const dotenv = require("dotenv");
 
 // setting config file
 dotenv.config({ path: "Backend/config/config.env" });
+// connect database
+connectDatabase();
 
 app.listen(process.env.PORT, () => {
   console.log(
