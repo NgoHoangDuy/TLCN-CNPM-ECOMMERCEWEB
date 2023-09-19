4e4d67a7e234fd0cc76de80cfca6258d9f0b32ac
duyhungg <dongocduyhung7@gmail.com>
2023-09-19 13:47:13 +0700

    update feature pagination,search,filter

--- tlcn-cnpm-be/Backend/controllers/productController.js
+++ tlcn-cnpm-be/Backend/controllers/productController.js
@@ -1,23 +1,53 @@
 const Product = require("../models/product");
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
+const APIFeatures = require("../utils/apiFeatures");
+
 // create new products test => api/v1/product/new
-exports.newProduct = async (req, res, next) => {
+exports.newProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.create(req.body);
 
   res.status(201).json({
     success: true,
     product,
   });
-};
+});
 //
 // Get single product details   =>   /api/v1/products?keyword=value
-exports.getProducts = async (req, res, next) => {
+exports.getProducts = catchAsyncErrors(async (req, res, next) => {
   const resPerPage = 4;
+  const productsCount = await Product.countDocuments();
 
-  let products = await Product.find();
+  const apiFeatures = new APIFeatures(Product.find(), req.query)
+    .search()
+    .filter();
+
+  let products = await apiFeatures.query;
+  let filteredProductsCount = products.length;
+
+  apiFeatures.pagination(resPerPage);
+  products = await apiFeatures.query.clone();
 
   res.status(200).json({
     success: true,
-    count: products.length,
+    productsCount,
+    resPerPage,
+    filteredProductsCount,
     products,
   });
-};
+});
+// delete product by admin => /api/v1?id=value
+exports.deleteProducts = catchAsyncErrors(async (res, req, next) => {
+  try {
+    let product = await Product.findById(req.params.id);
+    if (product) {
+      await product.deleteOne();
+      res.status(200).json({
+        success: true,
+        product,
+      });
+    } else {
+      return next(new ErrorHandler("Product not found", 404));
+    }
+  } catch (error) {}
+});
--- tlcn-cnpm-be/Backend/routes/product.js
+++ tlcn-cnpm-be/Backend/routes/product.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { getProducts, newProduct } = require("../controllers/productController");
+const {
+  getProducts,
+  newProduct,
+  deleteProducts,
+} = require("../controllers/productController");
 
 router.route("/products").get(getProducts);
 router.route("/product/new").post(newProduct);
-
+router.route("/product/delete/admin:id").delete(deleteProducts);
 module.exports = router;
