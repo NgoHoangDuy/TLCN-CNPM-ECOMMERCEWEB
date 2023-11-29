1248cc70fa34dede7aede2084e1c681e21989f70
duyhungg <dongocduyhung7@gmail.com>
2023-11-29 10:04:05 +0700

    update + fix bug

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,49 +1,41 @@
-const mongoose = require("mongoose");
-const validator = require("validator");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const crypto = require("crypto");
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please enter your name"],
-    maxLength: [30, "Your name cannot exceed 30 characters"],
-  },
-  email: {
-    type: String,
-    required: [true, "Please enter your email"],
-    unique: true,
-    validate: [validator.isEmail, "Please enter valid email address"],
-  },
-  password: {
-    type: String,
-    required: [true, "Please enter your password"],
-    minlength: [6, "Your password must be longer than 6 characters"],
-    select: false,
-  },
-  avatar: {
-    public_id: {
+const userSchema = new mongoose.Schema(
+  {
+    name: {
       type: String,
-      required: true,
+      required: [true, "Please enter your name"],
+      maxLength: [50, "Your name cannot exceed 50 characters"],
     },
-    url: {
+    email: {
       type: String,
-      required: true,
+      required: [true, "Please enter your email"],
+      unique: true,
     },
+    password: {
+      type: String,
+      required: [true, "Please enter your password"],
+      minLength: [6, "Your password must be longer than 6 characters"],
+      select: false,
+    },
+    avatar: {
+      public_id: String,
+      url: String,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
   },
-  role: {
-    type: String,
-    default: "user",
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  resetPasswordToken: String,
-  resetPasswordExpire: Date,
-});
-// // Encrypting password before saving user
+  { timestamps: true }
+);
+
+// Encrypting password before saving the user
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
     next();
@@ -52,73 +44,24 @@ userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, 10);
 });
 
-// // // Compare user password
-userSchema.methods.comparePassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
-
-// create JWT token
+// Return JWT Token
 userSchema.methods.getJwtToken = function () {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_TIME,
   });
 };
 
+// Compare user password
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 // Generate password reset token
 userSchema.methods.getResetPasswordToken = function () {
-  // Generate token// Encrypting password before saving user
-  userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) {
-      next();
-    }
-
-    this.password = await bcrypt.hash(this.password, 10);
-  });
-
-  // Compare user password
-  userSchema.methods.comparePassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password);
-  };
-
-  // Return JWT token
-  userSchema.methods.getJwtToken = function () {
-    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-      expiresIn: process.env.JWT_EXPIRES_TIME,
-    });
-  };
-
-  // Generate password reset token
-  userSchema.methods.getResetPasswordToken = function () {
-    // Generate token
-    const resetToken = crypto.randomBytes(20).toString("hex");
-
-    // Hash and set to resetPasswordToken
-    this.resetPasswordToken = crypto
-      .createHash("sha256")
-      .update(resetToken)
-      .digest("hex");
-
-    // Set token expire time
-    this.resetPasswordExpire = Date.now() + 30 * 60 * 1000;
-
-    return resetToken;
-  };
-  // tao ra token quen pass
-  userSchema.methods.getResetPasswordToken = function () {
-    // generate token
-    const resetToken = crypto.randomBytes(20).toString("hex");
-    // has and set to token
-    this.resetPasswordToken = crypto
-      .createHash("sha256")
-      .update(resetToken)
-      .digest("hex");
-    // set token expire time
-    this.resetPasswordExpire = Date.now() + 30 * 60 * 10000;
-    return resetToken.toString();
-  };
+  // Gernerate token
   const resetToken = crypto.randomBytes(20).toString("hex");
 
-  // Hash and set to resetPasswordToken
+  // Hash and set to resetPasswordToken field
   this.resetPasswordToken = crypto
     .createHash("sha256")
     .update(resetToken)
@@ -129,18 +72,5 @@ userSchema.methods.getResetPasswordToken = function () {
 
   return resetToken;
 };
-// tao ra token quen pass
-userSchema.methods.getResetPasswordToken = function () {
-  // generate token
-  const resetToken = crypto.randomBytes(20).toString("hex");
-  // has and set to token
-  this.resetPasswordToken = crypto
-    .createHash("sha256")
-    .update(resetToken)
-    .digest("hex");
-  // set token expire time
-  this.resetPasswordExpire = Date.now() + 30 * 60 * 10000;
-  return resetToken.toString();
-};
 
-module.exports = mongoose.model("User", userSchema);
+export default mongoose.model("User", userSchema);
--- Backend/utils/apiFilters.js
+++ Backend/utils/apiFilters.js
@@ -1,8 +1,9 @@
-class APIFeature {
+class APIFilters {
   constructor(query, queryStr) {
     this.query = query;
     this.queryStr = queryStr;
   }
+
   search() {
     const keyword = this.queryStr.keyword
       ? {
@@ -12,20 +13,22 @@ class APIFeature {
           },
         }
       : {};
+
     this.query = this.query.find({ ...keyword });
     return this;
   }
-  filter() {
+
+  filters() {
     const queryCopy = { ...this.queryStr };
 
-    // Removing fields from the query
-    const removeFields = ["keyword", "limit", "page"];
-    removeFields.forEach((el) => delete queryCopy[el]);
+    // Fields to remove
+    const fieldsToRemove = ["keyword", "page"];
+    fieldsToRemove.forEach((el) => delete queryCopy[el]);
 
     // Advance filter for price, ratings etc
     let queryStr = JSON.stringify(queryCopy);
     queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
-    console.log(queryStr);
+
     this.query = this.query.find(JSON.parse(queryStr));
     return this;
   }
@@ -38,4 +41,5 @@ class APIFeature {
     return this;
   }
 }
-module.exports = APIFeature;
+
+export default APIFilters;
