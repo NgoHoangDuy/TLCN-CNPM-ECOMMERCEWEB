6bcd55080da94a6e066abe023796cfeb9798723b
Ngô Hoàng Duy <ngohoangduy20@gmail.com>
2023-12-03 21:37:19 +0700

    admin-dashboard-charts-products

--- frontend/src/redux/api/orderApi.js
+++ frontend/src/redux/api/orderApi.js
@@ -13,6 +13,12 @@ export const orderApi = createApi({
         };
       },
     }),
+    myOrders: builder.query({
+      query: () => `/me/orders`,
+    }),
+    orderDetails: builder.query({
+      query: (id) => `/orders/${id}`,
+    }),
     stripeCheckoutSession: builder.mutation({
       query(body) {
         return {
@@ -22,8 +28,17 @@ export const orderApi = createApi({
         };
       },
     }),
+    getDashboardSales: builder.query({
+      query: ({ startDate, endDate }) =>
+        `/admin/get_sales/?startDate=${startDate}&endDate=${endDate}`,
+    }),
   }),
 });
 
-export const { useCreateNewOrderMutation, useStripeCheckoutSessionMutation } =
-  orderApi;
+export const {
+  useCreateNewOrderMutation,
+  useStripeCheckoutSessionMutation,
+  useMyOrdersQuery,
+  useOrderDetailsQuery,
+  useLazyGetDashboardSalesQuery,
+} = orderApi;
